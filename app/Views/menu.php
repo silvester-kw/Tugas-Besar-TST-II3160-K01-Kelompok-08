3a7bd7b3c5a9982dce20165521a4226608cfcfa9
Silvester Kresna W. P. P. <18221049@std.stei.itb.ac.id>
2023-12-16 22:03:33 +0700

    Menambahkan display data core dari sistem lain

--- app/Views/menu.php
+++ app/Views/menu.php
@@ -7,18 +7,18 @@
         <div class="px-8 md:px-16 lg:px-64 bg-blue-600 py-1">
             <div class="flex justify-between items-center px-4 py-2 bg-blue-600 text-white"> 
                 <div class="text-xl font-bold text-yellow-400">
-                    Takut.com
+                <a href="/">Takut.com</a>    
                 </div>
                 <div class=" flex space-x-2 pr-4 font-medium">
-                    <a href="/">Home</a>
+                    <a href="/" class="hover:underline hover:bg-yellow-600 px-2 py-1 rounded-md">Home</a>
                     <p>|</p> 
-                    <a href="/reserve">Reserve</a>
+                    <a href="/reserve" class="hover:underline hover:bg-yellow-600 px-2 py-1 rounded-md">Reserve</a>
                     <p>|</p> 
-                    <a href="/reserved">Reservation History</a> 
+                    <a href="/reserved" class="hover:underline hover:bg-yellow-600 px-2 py-1 rounded-md">Reservation History</a> 
                     <p>|</p> 
-                    <a href="/admin">Admin</a> 
+                    <a href="/admin" class="hover:underline hover:bg-yellow-600 px-2 py-1 rounded-md">Admin</a> 
                     <p>|</p> 
-                    <a href="/logout">Logout</a> 
+                    <a href="/logout" class="hover:underline hover:bg-yellow-600 px-2 py-1 rounded-md">Logout</a> 
                 </div>
             </div>
         </div>    
--- app/Views/wahana_api.php
+++ app/Views/wahana_api.php
@@ -2,13 +2,14 @@
 <html>
     <head>
         <link rel="stylesheet" href="<?= base_url() ?>css/styles.css?v=1.0">
+        <script src="<?= base_url('js/chart.js') ?>" ></script>
     </head>
     <body>
         <div class="px-8 md:px-16 lg:px-64">
-
-            <div class="text-3xl font-bold py-4 px-4 text-blue-400">
-                Wahana's Core
+            <div class="text-3xl text-center font-bold py-4 px-4 text-blue-600">
+                Wahana's Review
             </div>
+            <h2 class="text-xl font-bold mb-2">Table Rating</h2>
             <div class="container relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-300">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50">
@@ -17,23 +18,63 @@
                             <th  scope="col" class="px-6 py-3">Nama</th>
                             <th  scope="col" class="px-6 py-3">Rating</th>
                         </tr>
-            </thead>
-            <tbody>
-                <?php foreach($wahana as $analyticsData): ?>
-                    <tr class="odd:bg-white even:bg-gray-50 font-medium text-gray-900 whitespace-nowrap border-b">
-                    <td  scope="col" class="px-6 py-3"><?= esc($analyticsData['wahanaId']) ?></td>
-                    <td  scope="col" class="px-6 py-3"><?= esc($analyticsData['nama']) ?></td>
-                    <td  scope="col" class="px-6 py-3"><?= esc($analyticsData['rating']) ?></td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                    </thead>
+                    <tbody>
+                        <?php foreach($wahana as $analyticsData): ?>
+                            <tr class="odd:bg-white even:bg-gray-50 font-medium text-gray-900 whitespace-nowrap border-b">
+                            <td  scope="col" class="px-6 py-3"><?= esc($analyticsData['wahanaId']) ?></td>
+                            <td  scope="col" class="px-6 py-3"><?= esc($analyticsData['nama']) ?></td>
+                            <td  scope="col" class="px-6 py-3"><?= esc($analyticsData['rating']) ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="my-8 flex flex-row gap-8">
+            <div>
+                <h2 class="text-xl font-bold">Chart Rating</h2>
+                <div style="height: 300px; width: 600px; margin-top: 16px">
+                    <canvas id="ratingChart"></canvas>
+                </div>
+            </div>
+            </div>
+                <script>
+                    const ratingCtx = document.getElementById('ratingChart');
+
+                    let ratingData = [];
+                    let ratingLabels = [];
+                
+                    <?php if (!empty($wahana)): ?>
+                        <?php foreach ($wahana as $data):?>
+                            ratingLabels.push('<?= esc($data['nama']) ?>')
+                            ratingData.push('<?= esc($data['rating']) ?>')
+                        <?php endforeach ?>
+                        console.log({ratingLabels});
+                        new Chart(ratingCtx, {
+                            type: 'bar',
+                            data: {
+                                labels: ratingLabels,
+                                datasets: [{
+                                    label: 'Rating',
+                                    data: ratingData,
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                maintainAspectRatio: false,
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
+                                    }
+                                }
+                            }
+                        });
+                    <?php endif; ?>
+                </script>
+            <div class="mt-8">        
+                <a href="/reserve" class="rounded-md px-4 py-2 bg-blue-400 hover:bg-blue-600 font-medium text-white">Make Reservation</a>
+            </div>
         </div>
-        <div class="mt-8">
-            
-        <a href="/reserve" class="rounded-md px-4 py-2 bg-blue-400 hover:bg-blue-600 font-medium text-white">Make Reservation</a>
-    </div>
-</div>
-</body>
+    </body>
 </html>
 
